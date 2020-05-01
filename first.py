8286e81e6a6918338dce5f84172ddc9fc1d5f53a
tellurireddy <tellurireddy@gmail.com>
2020-05-01 05:10:16 -0400

    My python programs

---
+++ first.py
@@ -0,0 +1,7 @@
+#first python program
+
+for n in range(0, 5):
+    for i in range(1, n+1):
+        print("*", end=' ')
+    print(" ")
+
---
+++ second.py
@@ -0,0 +1,6 @@
+#second python program
+
+n=int(input('enter the value: '))
+for i in range(1, 11):
+    print(n, '/', '=', n/i)
+
